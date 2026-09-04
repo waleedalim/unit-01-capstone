64a3b7e099468f3236aeb468ffe3746c6fa07d29
waleedalim <waleed@waleedalim.com>
2026-09-04 12:29:51 -0400

    deployed backend to EC2 and frontend to S3

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "http://ec2-100-60-71-190.compute-1.amazonaws.com:3000/api", 
 });
 
 api.interceptors.request.use((config) => {
